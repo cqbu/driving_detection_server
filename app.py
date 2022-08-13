6fa42be90e2dd5b55d584fcf22ece9a89495ba5a
cqbu <20281103@bjtu.edu.cn>
2022-08-14 02:11:51 +0800

    route-download

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request,send_from_directory
 import os
 
 app = Flask(__name__)
@@ -27,13 +27,21 @@ def upload_ld_model():
     file = request.files.get('file')
     file.save(os.path.join('./weights/CLRNet', file.filename))
     return 'Upload successfully'
+@app.route('/download/<string:image_name>', methods=['GET'])
+def download(image_name):
+    image_name = os.path.basename(image_name)
+    image_name=image_name.split(".")[0]
+    image_name=image_name+".jpg"
+    if os.path.isfile(os.path.join("videos", image_name)):
+        return send_from_directory("videos", image_name)
+    pass
 
 if __name__ == '__main__':
     if not os.path.exists('./videos'):
         os.makedirs('./videos')
     if not os.path.exists('./weights/CLRNet'):
-        os.makedirs('./videos')
+        os.makedirs('./weights/CLRNet')
     if not os.path.exists('./weights/yolov5'):
-        os.makedirs('./videos')
+        os.makedirs('./weights/yolov5')
     
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5001, debug=True)
